688769ed4b5df2f80e42d662365eecc248d670f6
Dr. Ralf Berger <rberger@gk-software.com>
2018-09-19 18:06:57 +0200

    Created a first exporter service.

--- worker/exporter/index.js
+++ worker/exporter/index.js
@@ -1,3 +1,14 @@
+/* global process */
 const logger = require("../../common/logger")
+const Exporter = require("./exporter")
+const config = require("../../config/exporter")
 
 logger.info("Hello Exporter!")
+try {
+    const exporter = Exporter(config)
+    exporter.start()
+}
+catch (error) {
+    logger.error("exporter: " + error)
+    process.exit(1)
+}
