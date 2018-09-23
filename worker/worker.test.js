63cbf79156e1dcdd84454092852eaf726861075b
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2018-09-23 15:39:18 +0200

    Completed tests for pure Worker.

--- worker/worker.test.js
+++ worker/worker.test.js
@@ -1,5 +1,7 @@
-/* global fail test */
+/* global fail test setImmediate */
 const Worker = require("./worker")
+const fs = require("fs")
+const rimraf = require("rimraf")
 
 test("should be possible to create a worker.", () => {
     new Worker({"limit": 100})
@@ -19,4 +21,25 @@ test("should not be possible to start, without redefining the start method.", ()
         worker.start()
     }
     catch(error) { /* Works as designed. */ }
+})
+
+test("should be possible to close a not initialized worker.", (done) => {
+    const worker = new Worker({"limit": 100})
+    worker.close().then(done)
+})
+
+test("should create a testdb directory.", (done) => {
+    fs.mkdirSync("./testdb")
+    setImmediate(done)
+})
+
+test("should close a database, if created.", (done) => {
+    const worker = new Worker({"limit": 100})
+    worker.init("./testdb/test")
+    worker.close().then(done)
+})
+
+test("should delete the testdb directory.", (done) => {
+    rimraf.sync("./testdb")
+    setImmediate(done)
 })
