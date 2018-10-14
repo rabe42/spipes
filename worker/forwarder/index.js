35e4b30d2613c7c99fde01df778979f758a64e9b
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2018-10-14 21:42:21 +0200

    Basic index.js.

--- worker/forwarder/index.js
+++ worker/forwarder/index.js
@@ -1,12 +1,19 @@
-const http2 = require("http2")
-const winston = require("winston")
+/* global process */
+const Forwarder = require("./forwarder")
+const logger = require("../../common/logger")
+const config = require("../../config/exporter")
 
 /**
  * This is basically a http client, which is reading some configuration and trying to read
  * topics from the configured databases and forward it to the configured communication partners.
  */
 
-const client = http2.connect("https://localhost:3000/")
-
-client.on("error", (err) => { winston.log("error", err) })
-
+logger.info("Hello Forwarder!")
+try {
+    const forwarder = Forwarder(config)
+    forwarder.start()
+}
+catch (error) {
+    logger.error("exporter: " + error)
+    process.exit(1)
+}
