e7c65f2d70ba8380e96af891eed6d3accc61a621
Dr. Ralf Berger <rberger@gk-software.com>
2018-10-31 13:27:17 +0100

    Storing bookkeeping and first message.

--- common/message-sender.js
+++ common/message-sender.js
@@ -1,5 +1,7 @@
 const logger = require("./logger")
 const Joi = require("joi")
+const Promise = require("promise")
+const PouchDB = require("pouchdb")
 
 const hostSchema = Joi.object().keys({
     "host": Joi.string().hostname().required(),
@@ -23,15 +25,106 @@ const configSchema = Joi.object().keys({
  * @author Dr. Ralf Berger (c) 2018
  */
 class MessageSender {
-    constructor(config) {
 
+    constructor(config) {
         Joi.validate(config, configSchema, (err) => {
             if (err !== null) {
-                logger.error(`exporter.validateConfiguration(): "${err}"`)
+                logger.error(`MessageSender(): "${err}"`)
                 throw new Error(`Error during validation of the configuration: ${err}`)
             }
         })
         this.config = config
+        this.messageDb = new PouchDB(config["database-url"] + "/messages")
+    }
+
+    _initializeBookkeeping() {
+        logger.debug("MessageSender._initializeBookkeeping()")
+        const that = this
+        this.bookkeepingDb = new PouchDB(this.config["database-url"] + "/bookkeeping")
+        return new Promise((resolve, reject) => {
+            this.bookkeepingDb.get("sequence-no").then((data) => {
+                logger.debug("MessageSender._initializeBookkeeping(): Successful retrieved a sequence number from history.")
+                resolve(data)
+            }).catch(() => {
+                logger.debug("MessageSender._initializeBookkeeping(): Setting up new sequence number document...")
+                that.bookkeepingDb.put({
+                    _id: "sequence-no",
+                    "sequence-no": 0
+                }).then(() => {
+                    logger.debug("MessageSender._initializeBookkeeping(): Successful setup of new sequence number.")
+                    return that.bookkeepingDb.get("sequence-no").then((data) => {
+                        logger.debug("MessageSender._initializeBookkeeping(): New sequence-no successful retrieved.")
+                        that.sequenceNoData = data
+                        resolve(data)
+                    })
+                }).catch((error) => {
+                    logger.error(`MessageSender._initializeBookkeeping(): Error setting up new sequence number: ${error}`)
+                    reject(error)
+                })
+            })
+        })
+    }
+
+    /**
+     * Increments the sequence number not only in the receiver, but also in the database.
+     * @returns A promise, which resolves, if it was possible to update the database.
+     */
+    incrementSequenceNo() {
+        this.sequenceNoData["sequence-no"]++
+        return this.bookkeepingDb.put(this.sequenceNoData)
+    }
+
+    /**
+     * Wraps the message in the message envelope, store it into the queue database and trigger to send it.
+     * @param {*} message The message to be send.
+     * @returns A promise, which succeeds, as soon as the message is in the queue database.
+     */
+    send(destination, topic, message) {
+        const that = this
+        debugger
+        return this._wrapMessage(destination, topic, message).then((wrappedMessage) => {
+            return that._saveMessage(wrappedMessage)
+        })
+    }
+
+    _wrapMessage(destination, topic, message) {
+        const that = this
+        return new Promise((resolve, reject) => {
+            const wrappedMessage = {
+                originator: this.config["originator"],
+                destination: destination,
+                topic: topic,
+                message: message
+            }
+            if (that.bookkeepingDb) {
+                that._setSequenceNo(wrappedMessage, resolve, reject)
+            }
+            else {
+                that._initializeBookkeeping().then(() => {
+                    that._setSequenceNo(wrappedMessage, resolve, reject)
+                }).catch((error) => {
+                    logger.error(`MessageSender._wrapMessage(): failed to initialize bookkeeping due to: ${error}`)
+                })
+            }
+        })
+    }
+
+    _setSequenceNo(wrappedMessage, resolve, reject) {
+        debugger
+        logger.debug(`MessageSender._setSequenceNo(): ${this.sequenceNoData["sequence-no"]}`)
+        wrappedMessage["_id"] = `${this.config["originator"]}-${this.sequenceNoData["sequence-no"]}`
+        wrappedMessage["sequence-no"] = this.sequenceNoData["sequence-no"]
+        this.incrementSequenceNo().then(() => {
+            logger.debug("MessageSender._setSequenceNo(): successful.")
+            resolve(wrappedMessage)
+        }).catch((error) => {
+            logger.error(`MessageSender._setSequenceNo(): Not able to set sequence number because of: ${error}`)
+            reject(error)
+        })
+    }
+
+    _saveMessage(messageDocument) {
+        return this.messageDb.put(messageDocument)
     }
 }
 
--- common/message-sender.test.js
+++ common/message-sender.test.js
@@ -1,4 +1,7 @@
-/* global test */
+/* global afterAll beforeAll fail test */
+const fs = require("fs")
+const rimraf = require("rimraf")
+
 /**
  * Tests the sender class, which provides a simple way to send messages into
  * the infrastructure.
@@ -6,6 +9,25 @@
 const MessageSender = require("./message-sender")
 const config = require("../config/sender")
 
+beforeAll((done) => {
+    fs.mkdir(config["database-url"], done)
+})
+
+afterAll((done) => {
+    rimraf(config["database-url"], done)
+})
+
+let sender
+
 test("Should be possible to create a message sender.", () => {
-    new MessageSender(config)
+    sender = new MessageSender(config)
+})
+
+test("Should store the message and information into the queue database.", (done) => {
+    sender.send("destination", "topic", {name: "payload"}).then(() => {
+        done()
+    }).catch((error) => {
+        fail(error)
+        done()
+    })
 })
