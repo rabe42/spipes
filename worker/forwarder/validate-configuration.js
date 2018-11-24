432c29bc3a3a629abae2a1bdc013baa5153ff175
Dr. Ralf Berger <dr.ralf.berger@gmail.com>
2018-11-24 16:18:39 +0100

    Fixed Unit-Tests for forwarder after refactoring.

--- worker/forwarder/validate-configuration.js
+++ worker/forwarder/validate-configuration.js
@@ -10,7 +10,7 @@ const hostSchema = Joi.object().keys({
 const configSchema = Joi.object().keys({
     "name": Joi.string().hostname().required(),
     "topic": Joi.string().required(),
-    "hosts": Joi.array().items(hostSchema).min(1).unique().required(),
+    "host": hostSchema,
     "database-url": Joi.string().required(),
     "limit": Joi.number().integer().min(1).max(65000),
     "interval": Joi.number().integer().min(10),
