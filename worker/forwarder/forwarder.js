3b2abfef5d500cf9b51ca72a721b9ef750b91b30
Dr. Ralf Berger <rberger@gk-software.com>
2019-02-07 17:11:26 +0100

    Limiting the number of documents to process.

--- worker/forwarder/forwarder.js
+++ worker/forwarder/forwarder.js
@@ -33,11 +33,13 @@ class Forwarder extends Worker {
      */
     createH2ClientSession() {
         const hostDefinition = this.config.host
+        let that = this
         this.h2session = http2.connect(
             `https://${hostDefinition.host}:${hostDefinition.port}`,
             {ca: hostDefinition.certificate})
         this.h2session.on("error", (err) => {
             logger.error(`Forwarder.createH2ClientSession(${this.config.topic}): error - Exiting h2 client with error: ${err}`)
+            that.h2session.close()
         })
         this.h2session.on("close", () => {
             logger.info(`Forwarder.createH2ClientSession(${this.config.topic}): close - exiting h2 client.`)
@@ -50,9 +52,8 @@ class Forwarder extends Worker {
     start() {
         logger.debug("Forwarder.start()")
         const that = this
-        // TODO: Read only until the limit.
         // Read the configured "limit" messages for the configured topic from the database.
-        this.db.allDocs().then((result) => {
+        this.db.allDocs({"limit": this.config.limit}).then((result) => {
             for (let i = 1; i < result.rows.length; i++) {
                 that.processMessage(result.rows[i]._id)
             }
@@ -68,7 +69,7 @@ class Forwarder extends Worker {
      * Reads the real content of the message and send it to an receiver, if successful.
      */
     processMessage(messageId) {
-        logger.debug("Forwarder.processMessage(): originator")
+        logger.debug("Forwarder.processMessage(): begin")
         const that = this
         return this.db.get(messageId).then((message) => {
             // Forward them to the receiver.
